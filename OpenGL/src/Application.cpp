05f0cd2679888cb1d9c97acda606b97f52593059
zzc96 <56128130+zzc96@users.noreply.github.com>
2022-06-05 14:44:32 +0800

    缓冲区和布局的抽象

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -8,6 +8,7 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 struct ShaderProgramSource
 {
@@ -134,10 +135,11 @@ int main(void)
 		GLCall(glGenVertexArrays(1, &vao));
 		GLCall(glBindVertexArray(vao));
 
+		VertexArray va;
 		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
-
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FLOAT, sizeof(float) * 2, 0);
+		VertexBufferLayout layout;
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
 
@@ -166,7 +168,7 @@ int main(void)
 			GLCall(glUseProgram(shader));
 			GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-			GLCall(glBindVertexArray(vao));
+			va.Bind();
 			ib.Bind();
 
 			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
---
+++ OpenGL/src/VertexArray.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+
+class VertexArray
+{
+public:
+	VertexArray();
+	~VertexArray();
+
+	void Bind() const;
+	void Unbind() const;
+
+
+	void AddBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+
+private:
+	unsigned int m_RendererID;
+};
